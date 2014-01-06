9dbde8e3c72e049829004930e88cafb09067ac55
procload <ryan@ryanmerrill.net>
2014-01-06 14:14:04 -0500

    Add directory color config

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,7 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# make search up and down work, so partially type and hit up/down to find relevant stuff
+bindkey '^[[A' up-line-or-search                                                
+bindkey '^[[B' down-line-or-search
