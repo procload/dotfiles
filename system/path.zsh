7e65de87bf5ab031f1745ef0bb4a087203c175bb
procload <ryan@ryanmerrill.net>
2017-02-27 11:38:33 -0500

    Racket in Path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/Applications/Racket v6.2.1/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/Applications/Racket v6.8/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
