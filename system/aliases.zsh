f27cc9f2a4731bb93cb0c113c5f88534485b0326
Ryan Merill <me@ryan.cm>
2017-08-08 13:03:22 -0400

    Adds a bunch of stuff

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,4 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
+# This will make a new command called freewifi that changes you MAC address to
+#a random value. Note that to reset your MAC address, you'll either need to
+# reboot, or take a backup of it first with: ifconfig en0 | grep ether
+alias freewifi="sudo ifconfig en0 ether `openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'`"
--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,9 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/Applications/Racket v6.8/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/Applications/Racket v6.9/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-source ~/.rvm/scripts/rvm
+export http_proxy=http://proxy.int.8451.com:80
+export https_proxy=http://proxy.int.8451.com:443
+export ALL_PROXY=$http_proxy
+export PATH="$HOME/npm-global/bin:$PATH"
+export no_proxy="localhost, 127.0.0.1, 10.0.0.0/8, int.8451.com, dunnhumby.co.uk, artifactory.int.8451.com"
