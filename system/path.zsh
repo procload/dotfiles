62469cef73fbc977979b176bfcd47a74c46acad6
Ryan Merrill <ryan.merrill@8451.com>
2019-01-22 15:34:02 -0500

    Removes some cruft

--- system/path.zsh
+++ system/path.zsh
@@ -2,8 +2,8 @@ export PATH="./bin:/usr/local/bin:/Applications/Racket v6.9/bin:/usr/local/sbin:
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-export http_proxy=http://proxy.int.8451.com:80
-export https_proxy=http://proxy.int.8451.com:443
+# export http_proxy=http://proxy.int.8451.com:80
+# export https_proxy=http://proxy.int.8451.com:443
 export ALL_PROXY=$http_proxy
 export PATH="$HOME/npm-global/bin:$PATH"
 export no_proxy="localhost, 127.0.0.1, 10.0.0.0/8, int.8451.com, dunnhumby.co.uk, artifactory.int.8451.com"
